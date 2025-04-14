00c36f30710a1cf28ee7708404989972c0277ecd
KV <kirfitai@gmail.com>
2025-04-14 23:28:16 +0300

    Добавление миграций для модели Conversion

---
+++ migrations/versions/3853d5e09725_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 3853d5e09725
+Revises: 53998c57c348, 72b9537e6a4b, 7e89cb2de74e
+Create Date: 2025-04-14 13:51:02.618747
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3853d5e09725'
+down_revision = ('53998c57c348', '72b9537e6a4b', '7e89cb2de74e')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
