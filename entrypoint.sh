5f9d95966dec93868138c10fcbde655b8d9c9408
KV <kirfitai@gmail.com>
2025-04-14 18:59:29 +0300

    Исправлен entrypoint.sh и обновлен init_db.py для включения модели конверсий

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ set -e
 python /app/init_db.py
 
 # Запускаем приложение через gunicorn
-exec gunicorn --bind 0.0.0.0:$PORT --workers 1 --log-level info run:app 
+exec gunicorn --bind 0.0.0.0:$PORT --workers 1 --log-level info run:app
