58e6343a0ccb8aefd6a8ebb2d077dec9965d9cd4
KV <kirfitai@gmail.com>
2025-04-18 13:44:24 +0300

    Добавлен файл-обертка facebook_token.py для обеспечения обратной совместимости

---
+++ app/models/facebook_token.py
@@ -0,0 +1,9 @@
+"""
+Модуль совместимости для старого кода, ожидающего facebook_token
+"""
+
+# Импортируем нужные классы из модуля token
+from app.models.token import FacebookToken, FacebookTokenAccount
+
+# Любые дополнительные функции или классы, которые могут потребоваться
+# ... 
